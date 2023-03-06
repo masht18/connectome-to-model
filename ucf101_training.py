3674f5891c7f13c55a9bd469272786f55a9f6ca7
mashbayar.tugsbayar <mashbayar.tugsbayar@cn-c020.server.mila.quebec>
2023-03-06 16:41:55 -0500

    ucf training rudimentary video only

--- ucf101_training.py
+++ ucf101_training.py
@@ -29,6 +29,13 @@ def str2bool(v):
 
 parser = argparse.ArgumentParser()
 
+# data processing hyperparams
+parser.add_argument('--frames_per_clip', type = int, default = 16)
+parser.add_argument('--mel_sample_rate', type = int, default = 2)
+parser.add_argument('--mel_channels', type = int, default = 64)
+parser.add_argument('--mel_stft_hopsize', type = int, default = 64)
+parser.add_argument('--mel_pad_length', type = int, default = 200)
+
 parser.add_argument('--cuda', type = bool, default = True, help = 'use gpu or not')
 parser.add_argument('--epochs', type = int, default = 10)
 parser.add_argument('--layers', type = int, default = 1)
@@ -37,6 +44,9 @@ parser.add_argument('--reps', type = int, default = 1)
 parser.add_argument('--topdown', type = str2bool, default = True)
 parser.add_argument('--connection_decay', type = str, default = 'ones')
 parser.add_argument('--return_bottom_layer', type = str2bool, default = False)
+
+parser.add_argument('--ucf_data', type = str, default = '/network/datasets/ucf101.var/ucf101_torchvision/UCF-101')
+parser.add_argument('--ucf_annot', type = str, default = '/home/mila/m/mashbayar.tugsbayar/datasets/ucf101_annot')
 
 parser.add_argument('--model_save', type = str, default = 'saved_models/audio_newdata.pt')
 parser.add_argument('--results_save', type = str, default = 'results/no_topdown_newdata.npy')
@@ -51,12 +61,45 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(42)
 print(device)
 
-# # 1: Prepare dataset
+#################################################
+# # 1: PREPARE DATASET
 print('Loading datasets')
 
-data_path='/home/mila/m/mashbayar.tugsbayar/datasets'
-train_data = datasets.UCF101(root=data_path, download=True, train=True, transform=T.ToTensor())
-test_data = datasets.UCF101(root=data_path, download=True, train=False, transform=T.ToTensor())
+tfs = T.Compose([
+            # TODO: this should be done by a video-level transfrom when PyTorch provides transforms.ToTensor() for video
+            # scale in [0, 1] of type float
+            T.Lambda(lambda x: x / 255.),
+            # reshape into (T, C, H, W) for easier convolutions
+            T.Lambda(lambda x: x.permute(0, 3, 1, 2)),
+            # rescale to the most common size
+            T.Lambda(lambda x: nn.functional.interpolate(x, (240, 320))),
+])
+
+#audio_transform = torchaudio.transforms.MelSpectrogram(args['mel_sample_size'], 
+#                                     hop_length=args['mel_stft_hopsize'], 
+#                                     n_fft=4*args['mel_channels'], 
+#                                     n_mels=args['mel_channels'])
+
+def custom_collate(batch):
+    filtered_batch = []
+    for video, audio, label in batch:
+        #mel = torch.log(audio_transform(audio)+1e-6)/2.0
+        
+        # pad or trim
+        #if mel.shape[-1]>=args['mel_pad_length']:
+        #    mel = mel[:,:,:args['mel_pad_length']]
+        #else:
+        #    mel = pad_tensor(mel, args['mel_pad_length'], -1, pad_val=np.log(1e-6)/2.0)
+            
+        filtered_batch.append((video, label))
+    return torch.utils.data.dataloader.default_collate(filtered_batch)
+
+train_data = datasets.UCF101(args['ucf_data'], args['ucf_annot'], 
+                             frames_per_clip=args['frames_per_clip'],
+                            train=True, transform=tfs)
+test_data = datasets.UCF101(args['ucf_data'], args['ucf_annot'], 
+                             frames_per_clip=args['frames_per_clip'],
+                            train=False, transform=tfs)
 
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=32, shuffle=True)
@@ -102,10 +145,10 @@ def test_sequence(dataloader, clean_data, dataset_ref):
         for i, data in enumerate(dataloader, 0):
             optimizer.zero_grad()
 
-            imgs, audio, label = data
-            imgs, label = imgs.to(device), label.to(device)
+            video, label = data
+            video, label = video.to(device), label.to(device)
                 
-            output = model([imgs])
+            output = model([video])
 
             _, predicted = torch.max(output.data, 1)
             total += label.size(0)
@@ -122,21 +165,13 @@ def train_sequence():
     for i, data in enumerate(train_loader, 0):
         optimizer.zero_grad()
             
-        imgs, audio, label = data
-        imgs, label = imgs.to(device), label.to(device)
-
-        input_seqs = sequence_gen(imgs, label, train_data, mnist_ref_train, seq_style='addition').float()
+        video, label = data
+        video, label = video.to(device), label.to(device)
 
         # Generate random topdown for testing purposes only
-        topdown = torch.rand(imgs.shape[0], input_seqs.shape[1], args['topdown_c'], args['topdown_h'], args['topdown_w']).to(device)
+        #topdown = torch.rand(imgs.shape[0], input_seqs.shape[1], args['topdown_c'], args['topdown_h'], args['topdown_w']).to(device)
         
-        input_list = []
-        #imgs = torch.unsqueeze(imgs, 1)
-        #input_list.append(imgs)
-        input_list.append(input_seqs)
-        #input_list.append(topdown)
-        
-        output = model(input_list)
+        output = model([video])
             
         loss = criterion(output, label)
         running_loss += loss.item()

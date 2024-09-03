3d3c6e02be29b588d6cb94dd23b4ec5b077ad4ae
mashbayar.tugsbayar <mashbayar.tugsbayar@cn-a004.server.mila.quebec>
2024-09-03 18:40:17 -0400

    updated simple_training.py

--- scripts/simple_training.py
+++ scripts/simple_training.py
@@ -16,7 +16,9 @@ from torch import optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
-from model.graph import Graph, Architecture
+from connectome_to_model.utils.datagen import *
+from connectome_to_model.model.graph import Graph, Architecture
+from connectome_to_model.model.readouts import ClassifierReadout
 
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
@@ -28,7 +30,7 @@ def str2bool(v):
 
 
 
-def test_sequence(dataloader, clean_data):
+def test_sequence(dataloader):
     
     '''
     Inference
@@ -46,11 +48,10 @@ def test_sequence(dataloader, clean_data):
             optimizer.zero_grad()
 
             imgs, label = data
-            imgs, label = imgs.to(device), label.to(device)
+            imgs, label = torch.unsqueeze(imgs, 1).to(device), label.to(device)
                 
-            input_list = [torch.unsqueeze(imgs, 1)]
-
-            output = model(input_list)
+            y = model([imgs])
+            output = readout(y[0])
 
             _, predicted = torch.max(output.data, 1)
             total += label.size(0)
@@ -61,15 +62,14 @@ def test_sequence(dataloader, clean_data):
 def train_sequence():
     running_loss = 0.0
         
-    for i, data in enumerate(train_loader, 0):
+    for i, data in enumerate(trainloader, 0):
         optimizer.zero_grad()
             
         imgs, label = data
-        imgs, label = imgs.to(device), label.to(device)
+        imgs, label = torch.unsqueeze(imgs, 1).to(device), label.to(device)
         
-        input_list = [torch.unsqueeze(imgs, 1)]
-        
-        output = model(input_list)
+        y = model([imgs])
+        output = readout(y[0])
             
         loss = criterion(output, label)
         running_loss += loss.item()
@@ -88,8 +88,8 @@ if __name__ == "__main__":
     parser.add_argument('--seed', type = int, default = 42)
     parser.add_argument('--reps', type = int, default = 1, help = 'how many times to view input')
     parser.add_argument('--topdown', type = str2bool, default = True)
-    parser.add_argument('--graph_loc', type = str, default = '/home/mila/m/mashbayar/convgru_feedback/graphs/test/topdown_test_mult_only.csv')
-    parser.add_argument('--data_path', type = str, default = '/home/mila/m/mashbayar/datasets')
+    parser.add_argument('--graph_loc', type = str, default = '/home/mila/m/mashbayar.tugsbayar/convgru_feedback/graphs/test/topdown_test_mult_only.csv')
+    parser.add_argument('--data_path', type = str, default = '/home/mila/m/mashbayar.tugsbayar/datasets')
 
     parser.add_argument('--model_save', type = str, default = 'topdowna.pt')
     parser.add_argument('--results_save', type = str, default = 'topdown_only_noupsampling_mult.npy')
@@ -112,48 +112,50 @@ if __name__ == "__main__":
     train_data = datasets.MNIST(root=MNIST_path, download=True, train=True, transform=transform)
     test_data = datasets.MNIST(root=MNIST_path, download=True, train=False, transform=transform)
 
-    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_data, batch_size=32, shuffle=True)
+    trainloader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=32)
+    testloader = DataLoader(test_data, batch_size=32, shuffle=True, num_workers=32)
 
     #---------------------------------------------------
     ## 2: Load graph, choose input and output areas
-
-    input_nodes = [0]   # Which areas do inputs go to, can be multiple
-    output_node = 1     # Which area do you get output from, must be a single area
+    
+    input_nodes = [0]              # Which areas do inputs go to, can be multiple
+    output_node = 2                # Which area do you get output from
     graph_loc = args['graph_loc']
-    graph = Graph(graph_loc, input_nodes=input_nodes, output_node=output_node)
-
+    graph = Graph(graph_loc, input_nodes=input_nodes, output_nodes=output_node)
+    
     input_sizes = graph.find_input_sizes()    # Expected height, width of input for each area
     input_dims = graph.find_input_dims()      # Expected channel dimensions of input for each area 
 
     #-----------------------------------------------------
     ## 3: Initialize neural network
-    model = Architecture(graph, input_sizes, input_dims,
-                        topdown=args['topdown']).cuda().float()
+    model = Architecture(graph, input_sizes, input_dims).cuda().float()
+    readout = ClassifierReadout(model.output_sizes[0], n_classes=10).cuda().float()
 
-    # Optimizer & loss function
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    params = [{'params': model.parameters(), 'lr': 0.001},
+              {'params': readout.parameters(), 'lr': 0.001}]
+
+    optimizer = optim.Adam(params)
     criterion = nn.CrossEntropyLoss()
 
-    losses = {'loss': [], 'train_acc': [], 'val_acc': []}    
+    losses = {'loss': [], 'train_acc': [], 'match_acc': []}    
 
     if os.path.exists(args['model_save']):
         model.load_state_dict(torch.load(args['model_save']))
-        print("Loading existing model")
+        print("Loading existing ConvGRU model")
     else:
         print("No pretrained model found. Training new one.")
 
     for epoch in range(args['epochs']):
-        train_acc = test_sequence(train_loader, train_data)
-        val_acc = test_sequence(test_loader, test_data)
+        train_acc = test_sequence(trainloader)
+        match_acc = test_sequence(testloader)
         loss = train_sequence()
 
-        printlog = '| epoch {:3d} | running loss {:5.4f} | train accuracy {:1.5f} |val accuracy {:1.5f}'.format(epoch, loss, train_acc, val_acc)
+        printlog = '| epoch {:3d} | running loss {:5.4f} | train accuracy {:1.5f} |match accuracy {:1.5f}'.format(epoch, loss, train_acc, match_acc)
 
         print(printlog)
         losses['loss'].append(loss)
         losses['train_acc'].append(train_acc)
-        losses['val_acc'].append(val_acc)
+        losses['match_acc'].append(match_acc)
 
         with open(args['results_save'], "wb") as f:
             pickle.dump(losses, f)
